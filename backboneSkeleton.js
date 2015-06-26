377a38480864b6a4737e3d7cbc348580b332a7b9
Mikeronomicon <mikeronomicon@gmail.com>
2015-06-26 09:22:20 -0700

    finished backbone skeleton in 2:45 seconds. Probably needs to be error checked

---
+++ backboneSkeleton.js
@@ -0,0 +1,12 @@
+$(document).ready(function {
+	
+	var myModel = Backbone.Model.extend ({
+
+	})
+	var myView = Backbone.View.extend({
+
+	})
+	var myCollection = Backbone.Collection.extend({
+
+	})
+});
